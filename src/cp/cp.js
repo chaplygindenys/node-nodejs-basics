51d6cabceeecfadfe50c5a03547e24475e3df6a4
ChaplyginDenys <75256979+chaplygindenys@users.noreply.github.com>
2022-06-11 08:56:07 +0300

    Update cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,15 +1,1 @@
-import { spawn } from 'child_process';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-export const spawnChildProcess = async args => {
-  const sp = spawn('node', [__dirname + '/files/script.js', ...args]);
-
-  process.stdin.pipe(sp.stdin);
-  sp.stdout.pipe(process.stdout);
-};
-
-spawnChildProcess(['a', 2]);
+// Good day) I can not find you  in Discord . Send me please : Denys Chaplygin(chaplygindenys)  
